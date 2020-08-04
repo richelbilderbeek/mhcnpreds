8c33d1a38696c313007088e8c0835a71966d6acf
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-04 07:48:22 +0200

    Create LUTs

--- scripts/create_luts/create_luts.R
+++ scripts/create_luts/create_luts.R
@@ -30,69 +30,33 @@ haplotype_id <- args[3]
 message("haplotype_id: '", haplotype_id, "'")
 
 t_haplotype <- get_haplotype_lut()
-haplotype <- t$haplotype[t_haplotype$id == haplotype_id]
+haplotype <- t_haplotype$haplotype[t_haplotype$id == haplotype_id]
+message("haplotype: ", haplotype)
 
 target_filename <- paste0(peptide_source, "_", haplotype_id, "_", peptide_length, ".csv")
 message("target_filename: '", target_filename, "'")
 
+n_peptides <- 10
+message("n_peptides: ", n_peptides)
 
-
-
-
-
-
-
+n_quantiles <- 10
+message("n_quantiles: ", n_quantiles)
 
 library(mhcnuggetsr)
 
-
-
-haplotypes <- c(
-  mhcnuggetsr::get_trained_mhc_1_haplotypes(),
-  mhcnuggetsr::get_trained_mhc_2_haplotypes()
+peptides <- replicate(
+  n = n_peptides,
+  mhcnpreds::create_random_peptide(length = peptide_length)
 )
 
-# 2 lengths, 2 peptides, all haplotypes: 51 minutes
-# 2 lengths, 10 peptides, all haplotypes: ?510 minutes
+mhcnuggets_options <- mhcnuggetsr::create_mhcnuggets_options(
+  mhc = haplotype
+)
+ic50s <- mhcnuggetsr::predict_ic50(
+  mhcnuggets_options = mhcnuggets_options,
+  peptides = peptides
+)$ic50
 
-peptide_lengths <- c(9, 10, 13) # peptide length in amino acids
-n_peptides <- 20 # per peptide length and per haplotype
+q <- mhcnpreds::convert_ic50s_to_quantiles(ic50s, n = 10)
 
-
-if (mhcnuggetsr::is_on_ci()) {
-  n_aases <- c(9, 10)
-  n_peptides <- 2
-}
-
-for (peptide_length in peptide_lengths) {
-
-  peptides <- replicate(
-    n = n_peptides,
-    bbbq::create_random_peptide(n_aas = peptide_length)
-  )
-
-  for (haplotype in haplotypes) {
-    filename <- paste0(haplotype, "_", peptide_length, ".csv")
-    filename <- stringr::str_replace_all(filename, ":", "_")
-    t <- tibble::tibble(
-      peptide = peptides,
-      ic50 = NA
-    )
-    for (i in seq_len(nrow(t))) {
-      peptide <- t$peptide[i]
-      mhcnuggets_options <- mhcnuggetsr::create_mhcnuggets_options(
-        mhc = haplotype
-      )
-      ic50 <- mhcnuggetsr::predict_ic50(
-        mhcnuggets_options = mhcnuggets_options,
-        peptides = peptide
-      )$ic50
-      testthat::expect_equal(1, length(ic50))
-      t$ic50[i] <- ic50
-    }
-    readr::write_csv(t, filename)
-
-
-  }
-}
-
+readr::write_csv(q, target_filename)
